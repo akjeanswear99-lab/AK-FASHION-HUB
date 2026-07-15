e4d902135eadffbc96cb3e65abcd92f5a011a7c9
akjeanswear99-lab <ak.jeans.wear99@gmail.com>
2026-07-15 16:33:28 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzqNhLw2tWteJvTZtN8m-XNqUbhQ-hQ5b8d6d761avk5gFScDalM-PAlQs-P4caDeE/exec";
+
+fetch(API_URL)
+.then(res => res.json())
+.then(data => {
+
+let html = "";
+
+for(let i=1;i<data.length;i++){
+
+html += `
+<div class="product">
+<h3>${data[i][2]}</h3>
+<p><b>UID:</b> ${data[i][0]}</p>
+<p><b>Color:</b> ${data[i][5]}</p>
+<p><b>Size:</b> ${data[i][6]}</p>
+<p><b>Price:</b> ₹${data[i][8]}</p>
+<p><b>Stock:</b> ${data[i][10]}</p>
+</div>
+`;
+
+}
+
+document.getElementById("products").innerHTML = html;
+
+});
